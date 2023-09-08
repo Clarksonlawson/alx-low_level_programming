d9310220565c4256851abd9e2bb5c14357159b1c
Clarkson <clarksonlawson1@gmail.com>
2023-09-08 15:48:03 -0700

    fhtygjgh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+    int digit;
+
+    for (digit = 0; digit <= 9; digit++) {
+        putchar(digit + '0');
+        if (digit < 9) {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
+}
+
