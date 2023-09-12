c2217d647eeb4443b96aee2b11996c25003d40a3
Clarkson <clarksonlawson1@gmail.com>
2023-09-12 16:14:27 -0700

    Automated commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,6 +8,7 @@ int function2(int param1, double param2);
 void print_alphabet_x10(void);
 int _islower(int c);
 void function3(char *str);
+int _isalpha(int c);
 void print_alphabet(void); /*  Function prototype declaration */
 #endif /* MAIN_H */
 
