e9d9d74d9ea03bd547575893e2577551c4df3d7b
Clarkson <clarksonlawson1@gmail.com>
2023-09-12 15:15:20 -0700

    Automated commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -15,15 +15,3 @@ void print_alphabet(void)
 
 	_putchar('\n');
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,6 @@ int _putchar(char c);
 void function1(void);
 int function2(int param1, double param2);
 void function3(char *str);
-
+void print_alphabet(void); /*  Function prototype declaration */
 #endif /* MAIN_H */
 
