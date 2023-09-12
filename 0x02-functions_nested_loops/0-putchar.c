86817c23556d18228da83eb0b144800f0acc0bad
Clarkson <clarksonlawson1@gmail.com>
2023-09-12 14:48:20 -0700

    hello

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/* 0-putchar.c */
+
+int _putchar(char c);  /* Prototype for _putchar function */
+
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return (0);
+}
+
