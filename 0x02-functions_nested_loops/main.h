15f80e78319af9799cf77628263cb6d62d1d1138
Clarkson <clarksonlawson1@gmail.com>
2023-09-12 14:52:25 -0700

    g

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+int _putchar(char c);
+void function1(void);
+int function2(int param1, double param2);
+void function3(char *str);
+
+#endif /* MAIN_H */
+
