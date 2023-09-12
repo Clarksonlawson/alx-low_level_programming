8e82196b727d3359dc6c98bcbc8403bd2de7ab0a
Clarkson <clarksonlawson1@gmail.com>
2023-09-12 16:53:30 -0700

    Automated commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,7 @@ int _isalpha(int c);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
 int print_sign(int n);
 void print_alphabet(void); /*  Function prototype declaration */
 #endif /* MAIN_H */
