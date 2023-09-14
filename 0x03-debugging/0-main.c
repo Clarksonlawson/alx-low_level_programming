761aede7eb848047683d41c989bb123d25754e17
Clarkson <clarksonlawson1@gmail.com>
2023-09-14 14:30:21 -0700

    t

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Tests the function that prints if an integer is positive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;  /* Change the value to 0 to test the function with 0 */
+	positive_or_negative(i);
+
+	return (0);
+}
+
