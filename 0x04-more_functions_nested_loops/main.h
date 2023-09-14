e5d169d2a50cddf1d6ecbde23f127b4fa991e79e
Clarkson <clarksonlawson1@gmail.com>
2023-09-14 04:57:54 -0700

    Automated commit

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+int _putchar(char c);
+void function1(void);
+int _isupper(int c);
+int function2(int param1, double param2);
+void print_alphabet_x10(void);
+int _islower(int c);
+void function3(char *str);
+int _isalpha(int c);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int print_sign(int n);
+void print_alphabet(void); /*  Function prototype declaration */
+#endif /* MAIN_H */
+
