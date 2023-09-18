d0d83ed08f6401b08278706df98490ec1baa4875
Clarkson <clarksonlawson1@gmail.com>
2023-09-18 11:39:45 -0700

    g

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to the null-terminated string.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
+
