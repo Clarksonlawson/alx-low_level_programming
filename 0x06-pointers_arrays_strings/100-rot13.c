357aaaf356a81b821b22cc3892fbb885ddefaed9
Clarkson <clarksonlawson1@gmail.com>
2023-09-22 11:57:54 -0700

    g

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+/**
+ * rot13 - Encodes a string using ROT13.
+ * @str: The input string.
+ *
+ * Return: A pointer to the resulting string.
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; alphabet[j]; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
+
