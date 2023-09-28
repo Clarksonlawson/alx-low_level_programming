973367957b66a0627b60969123da91ef972cfcf5
Clarkson <clarksonlawson1@gmail.com>
2023-09-28 13:45:34 -0700

    g

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,27 @@
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to check.
+ *
+ * Return: 1 if @s is a palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len / 2; i++)
+	{
+		if (s[i] != s[len - 1 - i])
+		{
+			return (0); /* Not a palindrome */
+		}
+	}
+
+	return (1); /* Palindrome */
+}
+
