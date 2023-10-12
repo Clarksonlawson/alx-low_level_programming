96271de177cb601442ce4f972b3b904831054772
Clarkson <clarksonlawson1@gmail.com>
2023-10-12 12:03:48 -0700

    mulkti

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_op_func - Selects the correct function to perform the operation
+ * @s: The operator passed as an argument
+ *
+ * Return: Pointer to the corresponding function, or NULL if not found
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	printf("Error\n");
+	exit(99);
+	return (NULL);
+}
+
