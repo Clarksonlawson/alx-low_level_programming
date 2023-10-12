8d9cf0be3c06a38d3b84c4ef6261ac782ce2d76e
Clarkson <clarksonlawson1@gmail.com>
2023-10-12 12:12:27 -0700

    g fgrfjgbv

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,30 +6,41 @@
  * main - Entry point
  * @argc: Argument count
  * @argv: Argument vector
- * Return: 0 on success
+ *
+ * Return: 0 on success, 98 on incorrect number of arguments,
+ *         99 on invalid operator, 100 on division/modulo by zero.
  */
+
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*func)(int, int);
+	int num1, num2, result;
+	int (*func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
 
-    func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if (func == NULL)
-        exit(99);
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    result = func(num1, num2);
-    printf("%d\n", result);
+	result = func(num1, num2);
+	printf("%d\n", result);
 
-    return 0;
+	return (0);
 }
 
